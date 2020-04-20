cba30d7d1b60039967141b0177f7cbcdc6906c05
Gerard Eklu <gerard_eklu@homedepot.com>
2020-04-19 21:59:35 -0400

    add and delete items

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import TodoList from "./components/todoList"
 import './App.css';
 
-function App() {
+const App = () => {
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
+
+  const handleChange = (e) => {
+    const newInputText = e.target.value;
+    setInputText(newInputText)  
+  }
+
+  const addItem = (e) => {
+    e.preventDefault();
+    setItems((prevItems) => {
+      return ([...prevItems, inputText])
+    })
+    setInputText("")
+  }
+
+  const deleteItem = (id) => {
+    setItems((prevItems) => {
+      return prevItems.filter((item, index) => {
+        return index !== id
+      })
+    })
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="container">
+        <div className="heading">
+          <h1>To-Do List</h1>
+        </div>
+        <div className="form">
+          <input type="text" onChange={handleChange} value={inputText} />
+          <button onClick={addItem}>
+            <span>Add</span>
+          </button>
+        </div>
+        <div>
+       <ul>
+         {items.map((item, i) => {
+           return <TodoList  key={i}  item={item} id={i} onDelete={deleteItem} />
+         })}
+       </ul>
+        </div>
+      </div>
     </div>
   );
 }
